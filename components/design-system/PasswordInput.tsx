55e5d51c928784b4e42c58c59f89bc3062333480
shoaibmiaan <151955583+shoaibmiaan@users.noreply.github.com>
2025-08-27 07:48:31 +0500

    feat: use password input component (#91)

---
+++ components/design-system/PasswordInput.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Input, InputProps } from './Input';
+
+export const PasswordInput: React.FC<InputProps> = (props) => {
+  const [show, setShow] = React.useState(false);
+
+  return (
+    <Input
+      {...props}
+      type={show ? 'text' : 'password'}
+      iconRight={
+        <button
+          type="button"
+          onClick={() => setShow((s) => !s)}
+          aria-label={show ? 'Hide password' : 'Show password'}
+          className="focus:outline-none"
+        >
+          <i className={`fas ${show ? 'fa-eye-slash' : 'fa-eye'}`} />
+        </button>
+      }
+    />
+  );
+};
