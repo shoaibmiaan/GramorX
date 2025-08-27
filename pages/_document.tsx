8d1b869d547b11d93a7f0c08a23c0f3bc76ff0f9
shoaibmiaan <151955583+shoaibmiaan@users.noreply.github.com>
2025-08-27 05:45:26 +0500

    feat: add service worker and offline queue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Roboto+Slab:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
         <Main />
