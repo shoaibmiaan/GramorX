1a33cb62832bb6d8ffb62491363855303d0d9df8
shoaibmiaan <151955583+shoaibmiaan@users.noreply.github.com>
2025-08-28 18:06:59 +0500

    fix: use named Pricing import (#178)

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,7 +1,7 @@
 // pages/pricing.tsx
 import * as React from 'react';
-import PricingSection from '@/components/sections/Pricing';
+import { Pricing as PricingSection } from '@/components/sections/Pricing';
 
 export default function PricingPage() {
-  return <PricingSection defaultCycle="monthly" showFree highlight="rocket" />;
+  return <PricingSection />;
 }
